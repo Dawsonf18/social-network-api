17ef16aed365f8219d6aa7efbeaecb5f84d45256
Dawson Filion <Dawsonfilion21@gmail.com>
2022-06-20 09:53:20 -0400

    User.js and dateFormat in utils

---
+++ models/User.js
@@ -0,0 +1,50 @@
+const { truncate } = require('fs');
+const { Schema, model, Types } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        userName: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: { 
+            type: String,
+            required: true,
+            match: /.+\@.+\..+/,
+            unique: true
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thoughts'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Friends'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        // prevents virtuals from creating duplicate of _id as `id`
+        id: false
+    }
+);
+
+// retrieves length of the users 'friends' array
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.reduce(
+        (total, friends) => total + friends.replies.length + 1,
+        0
+    );
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
